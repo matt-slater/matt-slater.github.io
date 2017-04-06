38e966d4b030c067b113907e7efb314f98125941
Matt Slater <synapserelapse@upper-echelon.home>
2017-04-06 12:53:45 -0400

    second post

--- js/core.js
+++ js/core.js
@@ -1,8 +1,10 @@
-var items, item, red, green, blue;
+var items, item, red, green, blue, yellow;
 red = "240, 128, 128";
 blue = "128, 184, 240";
-green = "153, 255, 204";
-items = [red, blue, green];
+green = "176, 187, 85";
+yellow = "246, 235, 97";
+
+items = [red, blue, green, yellow];
 item = items[Math.floor(Math.random() * items.length)];
 $(function () {
 
